4d283ed95edbb0ca827e5b54373365f94b226a08
ivan <ivan7shpytchuk@gmail.com>
2023-09-12 06:41:25 +0300

    lab 1

---
+++ lab-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        //зчитуєм вхідні дані з файлу INPUT.TXT
+        string[] input = File.ReadAllLines("INPUT.TXT");
+        int W = int.Parse(input[0].Split()[0]);
+        int H = int.Parse(input[0].Split()[1]);
+
+        // Вираховування кількості невироджених прямокутників
+        long result = CountRectangles(W, H);
+
+        // Записуєм результат у вихідний файл OUTPUT.TXT
+        File.WriteAllText("OUTPUT.TXT", result.ToString());
+    }
+
+    static long CountRectangles(int W, int H)
+    {
+
+        long s = 0;
+        for (long i = 1; i <= W; i++)
+        {
+            for (long j = 1; j <= H; j++)
+            {
+                s += i * j;
+            }
+        }
+
+        return s;
+    }
+}
