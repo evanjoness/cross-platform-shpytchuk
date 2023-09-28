522d8924d190a19bf8b7bb572d36e7d3652f0515
ivan <ivan7shpytchuk@gmail.com>
2023-09-28 09:05:59 +0300

    lab 1 86 variant

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -1,34 +1,53 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        //зчитуєм вхідні дані з файлу INPUT.TXT
+        // Читаємо вхідні дані з файлу INPUT.TXT
         string[] input = File.ReadAllLines("INPUT.TXT");
-        int W = int.Parse(input[0].Split()[0]);
-        int H = int.Parse(input[0].Split()[1]);
+        int n, k, p;
+        string w;
+        n = int.Parse(input[0].Split()[0]);
+        k = int.Parse(input[0].Split()[1]);
+        p = int.Parse(input[0].Split()[2]);
+        w = input[1];
+        Dictionary<char, string> morphism = new Dictionary<char, string>();
 
-        // Вираховування кількості невироджених прямокутників
-        long result = CountRectangles(W, H);
-
-        // Записуєм результат у вихідний файл OUTPUT.TXT
-        File.WriteAllText("OUTPUT.TXT", result.ToString());
-    }
-
-    static long CountRectangles(int W, int H)
-    {
-
-        long s = 0;
-        for (long i = 1; i <= W; i++)
+        for (int i = 2; i < 2 + n; i++)
         {
-            for (long j = 1; j <= H; j++)
-            {
-                s += i * j;
-            }
+            morphism[(char)('A' + i - 2)] = input[i];
         }
 
-        return s;
+        // Обчислюємо fk(w) для k разів
+        for (int i = 0; i < k; i++)
+        {
+            string newW = "";
+            foreach (char c in w)
+            {
+                if (morphism.ContainsKey(c))
+                {
+                    newW += morphism[c];
+                }
+                else
+                {
+                    newW += c;
+                }
+            }
+            w = newW;
+        }
+
+        // Виводимо p-й символ або "-" якщо він відсутній
+        if (p <= w.Length)
+        {
+            Console.WriteLine(w[p - 1]);
+            File.WriteAllText("OUTPUT.TXT", w[p - 1].ToString());
+        }
+        else
+        {
+            Console.WriteLine("-");
+        }
     }
 }
